08453758f325efa6383fcd87c9c2860c5e9b50a8
niviavinia <148988550+niviavinia@users.noreply.github.com>
2026-03-03 23:24:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', authRoutes); 
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected');
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch((err) => console.error('DB Connection Error:', err));
